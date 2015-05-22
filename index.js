c7994495d30b8d73e9bd8ae40acee30d9b10d220
Victor Perin <me@victorperin.com>
2015-05-22 17:20:46 -0300

    first test

---
+++ index.js
@@ -0,0 +1,19 @@
+var exec = require('child_process').exec;
+
+var textos = ["q?", "q", "Que?","ke?"];
+
+
+
+var child = exec('tweet stream "nasus" --json');
+child.stdout.on('data', function(data) {
+    //console.log('stdout: ' + data);
+    var dados = JSON.parse(data);
+    console.log("@"+dados.user.screen_name+": "+dados.text);
+    var textoAleatorio = textos[Math.floor(Math.random()*textos.length)];
+    var comando = 'tweet new "@'+dados.user.screen_name+' '+textoAleatorio+'"';
+    var nasus = exec(comando);
+    child.stdout.on('data', function(nasusData) {
+      console.log(comando);
+      console.log(nasusData);
+    });
+});
