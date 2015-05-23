fcc138a7a9e314884c71de00d3ece5619fb872d5
Victor Perin <victor_perin@nerdhitech.com.br>
2015-05-23 01:07:20 -0300

    corrigindo função de resposta...

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ child.stdout.on('data', function(data) {
     console.log("@"+dados.user.screen_name+": "+dados.text);
     var textoAleatorio = textos[Math.floor(Math.random()*textos.length)];
     var comando = 'tweet new "@'+dados.user.screen_name+' '+textoAleatorio+'"';
-    var nasus = exec(comando);
+    var nasusData = exec(comando);
+    console.log(comando);
     child.stdout.on('data', function(nasusData) {
-      console.log(comando);
       console.log(nasusData);
     });
 });
