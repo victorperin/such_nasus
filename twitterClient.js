cdd0765cb3538d2001706ce75d4aa23937956078
Victor Perin <victor.perin@princiweb.com.br>
2016-02-04 11:09:51 -0200

    fix(twitterClient): removing justin bieber!

--- twitterClient.js
+++ twitterClient.js
@@ -5,11 +5,18 @@ var configuration = {
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 }
 
+var configuration = {
+  consumer_key: '7TTyf7BCJylJiIcPzMtSJOTLJ',
+  consumer_secret: 'mI2kmo8fs9cqIwLYZRlgzisSdMhZXJFZLdrNQoytBCceW5oTEL',
+  access_token_key: '3293576481-BdDE4PiksYLXfPT8lHT8HeadeYiCsOemrmo9jNb',
+  access_token_secret: '8yCyMaB7kavGogoYC6OKRiRF6bmuXfts1bPOjHACytBQD'
+}
+
 var Twitter = require('twitter');
 var client = new Twitter(configuration);
 
 exports.stream = function (word, callback){
-  client.stream('statuses/filter', {track: 'justin'}, function(stream) {
+  client.stream('statuses/filter', {track: word}, function(stream) {
     stream.on('data', function(tweet) {
       callback(undefined, tweet);
       console.log(tweet.user.screen_name+':',tweet.text);
