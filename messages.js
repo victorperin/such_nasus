17b4c1cabae709565a1f9e6e960a6e6a351116da
Victor Perin <victor.perin@princiweb.com.br>
2016-02-04 10:43:07 -0200

    feat(messages): inserindo todas as falas

---
+++ messages.js
@@ -0,0 +1,82 @@
+var others = [
+  'q?',
+  'Q?'
+];
+
+var infernal = {
+  movement: [
+    "Each fork in your path leads to fire.",
+    "They toil to live in a fool's paradise.",
+    "Mortality is guilt. All will be judged.",
+    "Redemption is a hollow wish.",
+    "I am the final arbiter.",
+    "So eager to find meaning in nothingness.",
+    "I walk behind none.",
+    "Do storms pity the drowned?",
+    "Fate is a manifestation of my will.",
+    "Mortals are not meant to rule.",
+    "Hope is the opiate of the frail.",
+    "Mankind's corruption spreads no further.",
+    "Try not the temper of the ascended."
+  ],
+  attacking: [
+    "Sink into darkness.",
+    "Such hollow, empty minds.",
+    "Only the guilty flee.",
+    "This is the way your world ends.",
+    "Grace becomes fire.",
+    "Join the lost.",
+    "Ambition is a mirage.",
+    "Here falls the shadow of despair.",
+    "A fate, sealed in blood.",
+    "Cleave through body, and mind.",
+    "Filth of the earth."
+  ],
+  jokes: [
+    "Who's a good boy? I'm a good boy.",
+    "Now that I've ascended, I can have treats, any time.",
+    "Once you ascend, the old clothes won't fit any more."
+  ]
+};
+
+var classic = {
+  movement: [
+    "There, for grace, I go.",
+    "It is neverending.",
+    "Their death, awaits.",
+    "Do not try my patience.",
+    "Between life and death lies immortality.",
+    "Perhaps Shurima was meant to fall.",
+    "Some things must remain buried.",
+    "Eons... pass like days.",
+    "The sky was naught but dying stars.",
+    "For centuries, I have watched.",
+    "Burdens sleep best in their tombs.",
+    "No dawn comes without darkness.",
+    "I walk through the ages.",
+    "Shurima is not of this time.",
+    "Errant words may fell empires.",
+    "The past is a tapestry of what lies ahead.",
+    'The cycle of stack and farm continues. We will Q, they will die... Oh, wait...'
+  ],
+  jokes: [
+    "Who let the dogs out? Woof. Woof. Woof.",
+    "No, I will not fetch the ball.",
+    "My bite is worse than my bark."
+  ],
+  attacking: [
+    "Anthropomancy: divination by entrails.",
+    "I bring death.",
+    "Soon, there will be nothing.",
+    "Life is part of a cycle. Yours is over.",
+    "Your soul will be measured.",
+    "Death is a harsh mistress.",
+    "Your spirit is hollow.",
+    "Journey into the beyond.",
+    "Return to the dust.",
+    "We approach a time of reckoning.",
+    "This too must end.",
+    "Some spirits are fated to burn.",
+    "Eternity is beyond your reach."
+  ]
+};
